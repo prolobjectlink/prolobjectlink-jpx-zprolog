66bb87df1079bffb0ecd0d7fb6949b71d3e4f99c
Jose Zalacain <jzalacain@delnet.cu>
2018-12-12 15:10:39 +0100

    Update JPA related

---
+++ src/main/java/org/logicware/prolog/zprolog/ZPrologPersistenceProvider.java
@@ -0,0 +1,9 @@
+package org.logicware.prolog.zprolog;
+
+import javax.persistence.spi.PersistenceProvider;
+
+import org.logicware.database.jpa.JpaAbstractProvider;
+
+public class ZPrologPersistenceProvider extends JpaAbstractProvider implements PersistenceProvider {
+
+}
